dadf3340c882d22e40a74d15932860f2eb956b4e
MohanVivithsu <mohanvivithsu@gmail.com>
2025-09-08 15:51:55 +0530

    updated appspec.yml,deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
+AWS_ACCOUNT_ID=753548365490
+AWS_REGION=ap-south-1
+
 # Stop existing container if running
 docker stop userinfo-app || true
 docker rm userinfo-app || true
 
-# Pull latest image from ECR
-$(aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin <AWS_ACCOUNT_ID>.dkr.ecr.ap-south-1.amazonaws.com)
+# Login to ECR
+aws ecr get-login-password --region ${AWS_REGION} | \
+  docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+# Pull latest image
 docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/userinfo-app:latest
 
 # Run container
